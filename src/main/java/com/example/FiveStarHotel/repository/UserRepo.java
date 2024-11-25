a9e61c314a25a4d72e8f4730532c3e828d928a12
unknown <mcmanuel755@gmail.com>
2024-11-25 04:45:03 +0100

    adjustment

--- src/main/java/com/example/FiveStarHotel/repository/UserRepo.java
+++ src/main/java/com/example/FiveStarHotel/repository/UserRepo.java
@@ -1,10 +1,8 @@
 package com.example.FiveStarHotel.repository;
 
 import com.example.FiveStarHotel.model.User;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User,Long> {
