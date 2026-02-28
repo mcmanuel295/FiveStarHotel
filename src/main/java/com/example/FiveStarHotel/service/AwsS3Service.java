2e4bb9b314357492fceff35a6403ccdc98b4091b
unknown <mcmanuel755@gmail.com>
2026-02-28 06:36:42 +0100

    all

--- src/main/java/com/example/FiveStarHotel/service/AwsS3Service.java
+++ src/main/java/com/example/FiveStarHotel/service/AwsS3Service.java
@@ -1,9 +1,7 @@
 package com.example.FiveStarHotel.service;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
