349ffbaf9b4b71c6999c67f5dbfa64dd0ea70451
unknown <mcmanuel755@gmail.com>
2026-06-13 02:52:23 +0100

    maven spotless to repository

--- src/main/java/com/example/FiveStarHotel/repository/BookingRepo.java
+++ src/main/java/com/example/FiveStarHotel/repository/BookingRepo.java
@@ -1,13 +1,11 @@
 package com.example.FiveStarHotel.repository;
 
 import com.example.FiveStarHotel.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface BookingRepo extends JpaRepository<Booking,Long> {
+public interface BookingRepo extends JpaRepository<Booking, Long> {
 
     List<Booking> findByRoomId(Long roomID);
 
--- src/main/java/com/example/FiveStarHotel/repository/UserRepo.java
+++ src/main/java/com/example/FiveStarHotel/repository/UserRepo.java
@@ -1,14 +1,12 @@
 package com.example.FiveStarHotel.repository;
 
 import com.example.FiveStarHotel.model.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepo extends JpaRepository<User,Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 
     boolean existsByEmail(String email);
 
     Optional<User> findByEmail(String email);
-
 }
