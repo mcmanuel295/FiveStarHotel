be0b2b1f4e6d4c84db3d0305b7627b49d72bb0db
unknown <mcmanuel755@gmail.com>
2026-06-13 02:58:37 +0100

    maven spotless to util

--- src/main/java/com/example/FiveStarHotel/util/JWTUtils.java
+++ src/main/java/com/example/FiveStarHotel/util/JWTUtils.java
@@ -2,54 +2,52 @@ package com.example.FiveStarHotel.util;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import org.apache.commons.codec.digest.HmacAlgorithms;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
 import java.util.function.Function;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.apache.commons.codec.digest.HmacAlgorithms;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class JWTUtils {
 
-    private static final long EXPIRATION_TIME = 1000*60 *60*24*7;
-    private final SecretKey key ;
+    private static final long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 7;
+    private final SecretKey key;
 
     private String secretString = "";
 
     public JWTUtils() {
         String secretString = "reu3423iygidfhghugowea8fwe0e9r23952395934ycpqucny8q384tbvdgfiu";
         byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
-        this.key = new SecretKeySpec(keyBytes, HmacAlgorithms.HMAC_SHA_256+"");
+        this.key = new SecretKeySpec(keyBytes, HmacAlgorithms.HMAC_SHA_256 + "");
     }
 
-
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(key)
                 .compact();
     }
 
-    public String extractUsername(String token){
+    public String extractUsername(String token) {
         return extractClaims(token, Claims::getSubject);
     }
 
-    private <T> T extractClaims(String token, Function<Claims,T> claimsTFunction ){
-        return claimsTFunction.apply(Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
+    private <T> T extractClaims(String token, Function<Claims, T> claimsTFunction) {
+        return claimsTFunction.apply(
+                Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload());
     }
 
-
-    public boolean isValidToken(String token,UserDetails userDetail){
+    public boolean isValidToken(String token, UserDetails userDetail) {
         final String username = extractUsername(token);
-        return (username.equals(userDetail.getUsername()) &&( !isTokenExpired(token)));
+        return (username.equals(userDetail.getUsername()) && (!isTokenExpired(token)));
     }
 
     private boolean isTokenExpired(String token) {
-        return extractClaims(token,Claims::getExpiration).before(new Date());
+        return extractClaims(token, Claims::getExpiration).before(new Date());
     }
 }
--- src/main/java/com/example/FiveStarHotel/util/Utils.java
+++ src/main/java/com/example/FiveStarHotel/util/Utils.java
@@ -6,41 +6,38 @@ import com.example.FiveStarHotel.dto.UserDto;
 import com.example.FiveStarHotel.model.Booking;
 import com.example.FiveStarHotel.model.Room;
 import com.example.FiveStarHotel.model.User;
-
 import java.security.SecureRandom;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Utils {
 
-    private static final String ALPHANUMERIC_STRING= "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
     private static final SecureRandom secureRandom = new SecureRandom();
 
-    public static String generateRandomAConfirmationCode(int length){
+    public static String generateRandomAConfirmationCode(int length) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < length; i++) {
             int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
             char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
             stringBuilder.append(randomChar);
-
-    }
+        }
         return stringBuilder.toString();
-     }
+    }
 
+    public static UserDto mapUserEntityToUserDto(User user) {
+        UserDto userDto = new UserDto();
 
-     public static UserDto mapUserEntityToUserDto(User user){
-         UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        userDto.setRole(user.getRole());
+        return userDto;
+    }
 
-         userDto.setId(user.getId());
-         userDto.setName(user.getName());
-         userDto.setEmail(user.getEmail());
-         userDto.setPhoneNumber(user.getPhoneNumber());
-         userDto.setRole(user.getRole());
-         return  userDto;
-     }
-
-    public static RoomDto mapRoomEntityToRoomDto(Room room){
+    public static RoomDto mapRoomEntityToRoomDto(Room room) {
         RoomDto roomDto = new RoomDto();
 
         roomDto.setId(room.getId());
@@ -48,10 +45,10 @@ public class Utils {
         roomDto.setRoomPrice(room.getRoomPrice());
         roomDto.setRoomPhotoUrl(room.getRoomPhotoUrl());
         roomDto.setRoomDescription(room.getRoomDescription());
-        return  roomDto;
+        return roomDto;
     }
 
-    public static BookingDto  mapBookingsEntityToBookingsDto(Booking booking){
+    public static BookingDto mapBookingsEntityToBookingsDto(Booking booking) {
         BookingDto bookingDto = new BookingDto();
 
         bookingDto.setId(booking.getId());
@@ -64,7 +61,7 @@ public class Utils {
         return bookingDto;
     }
 
-    public static RoomDto mapRoomEntityToRoomDtoPlusBookings(Room room){
+    public static RoomDto mapRoomEntityToRoomDtoPlusBookings(Room room) {
         RoomDto roomDto = new RoomDto();
 
         roomDto.setId(room.getId());
@@ -74,65 +71,74 @@ public class Utils {
         roomDto.setRoomDescription(room.getRoomDescription());
 
         if (room.getBookings() != null) {
-            roomDto.setBookings(room.getBookings().stream().map(Utils::mapBookingsEntityToBookingsDto ).collect(Collectors.toList()));
-
+            roomDto.setBookings(
+                    room.getBookings().stream()
+                            .map(Utils::mapBookingsEntityToBookingsDto)
+                            .collect(Collectors.toList()));
         }
 
-        return  roomDto;
+        return roomDto;
     }
 
-     public static UserDto mapUserEntityToUseDtoPlusUserBookingsAndUserRoom(User user){
-         UserDto userDto = new UserDto();
-         userDto.setId(user.getId());
-         userDto.setName(user.getName());
-         userDto.setEmail(user.getEmail());
-         userDto.setPhoneNumber(user.getPhoneNumber());
-         userDto.setRole(user.getRole());
+    public static UserDto mapUserEntityToUseDtoPlusUserBookingsAndUserRoom(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+        userDto.setPhoneNumber(user.getPhoneNumber());
+        userDto.setRole(user.getRole());
 
-         if (!user.getBookings().isEmpty()) {
-             userDto.setBookings(user.getBookings().stream().map(booking -> mapBookingEntityToBookingDtoPlusBookedRooms(booking,false)).collect(Collectors.toList()));
-         }
-         return userDto;
-     }
+        if (!user.getBookings().isEmpty()) {
+            userDto.setBookings(
+                    user.getBookings().stream()
+                            .map(
+                                    booking ->
+                                            mapBookingEntityToBookingDtoPlusBookedRooms(
+                                                    booking, false))
+                            .collect(Collectors.toList()));
+        }
+        return userDto;
+    }
 
+    public static BookingDto mapBookingEntityToBookingDtoPlusBookedRooms(
+            Booking booking, boolean mapUser) {
+        BookingDto bookingDto = new BookingDto();
 
-     public static BookingDto mapBookingEntityToBookingDtoPlusBookedRooms(Booking booking, boolean mapUser){
-         BookingDto bookingDto = new BookingDto();
+        bookingDto.setId(booking.getId());
+        bookingDto.setCheckInDate(booking.getCheckInDate());
+        bookingDto.setCheckOutDate(booking.getCheckOutDate());
+        bookingDto.setNoOfAdults(booking.getNoOfAdults());
+        bookingDto.setNoOfChildren(booking.getNoOfChildren());
+        bookingDto.setTotalNumOfGuest(booking.getTotalNumOfGuest());
+        bookingDto.setBookingConfirmationCode(booking.getBookingConfirmationCode());
 
-         bookingDto.setId(booking.getId());
-         bookingDto.setCheckInDate(booking.getCheckInDate());
-         bookingDto.setCheckOutDate(booking.getCheckOutDate());
-         bookingDto.setNoOfAdults(booking.getNoOfAdults());
-         bookingDto.setNoOfChildren(booking.getNoOfChildren());
-         bookingDto.setTotalNumOfGuest(booking.getTotalNumOfGuest());
-         bookingDto.setBookingConfirmationCode(booking.getBookingConfirmationCode());
+        if (mapUser) {
+            bookingDto.setUser(Utils.mapUserEntityToUserDto(booking.getUser()));
+        }
+        if (booking.getRoom() != null) {
+            RoomDto roomDto = new RoomDto();
 
-         if (mapUser) {
-             bookingDto.setUser(Utils.mapUserEntityToUserDto(booking.getUser()));
-         }
-         if (booking.getRoom() != null) {
-             RoomDto roomDto = new RoomDto();
+            roomDto.setId(booking.getRoom().getId());
+            roomDto.setRoomType(booking.getRoom().getRoomType());
+            roomDto.setRoomPrice(booking.getRoom().getRoomPrice());
+            roomDto.setRoomPhotoUrl(booking.getRoom().getRoomPhotoUrl());
+            roomDto.setRoomDescription(booking.getRoom().getRoomDescription());
+            bookingDto.setRoom(roomDto);
+        }
+        return bookingDto;
+    }
 
-             roomDto.setId(booking.getRoom().getId());
-             roomDto.setRoomType(booking.getRoom().getRoomType());
-             roomDto.setRoomPrice(booking.getRoom().getRoomPrice());
-             roomDto.setRoomPhotoUrl(booking.getRoom().getRoomPhotoUrl());
-             roomDto.setRoomDescription(booking.getRoom().getRoomDescription());
-             bookingDto.setRoom(roomDto);
-         }
-         return bookingDto;
-     }
-
-     public static List<UserDto> mapUserListEntityToUserListDto(List<User> userList){
+    public static List<UserDto> mapUserListEntityToUserListDto(List<User> userList) {
         return userList.stream().map(Utils::mapUserEntityToUserDto).collect(Collectors.toList());
-     }
+    }
 
-    public static List<RoomDto> mapRoomListEntityToRoomListDto(List<Room> roomList){
+    public static List<RoomDto> mapRoomListEntityToRoomListDto(List<Room> roomList) {
         return roomList.stream().map(Utils::mapRoomEntityToRoomDto).collect(Collectors.toList());
     }
 
-    public static List<BookingDto> mapBookingListEntityToBookingListDto(List<Booking> bookingList){
-        return bookingList.stream().map(Utils::mapBookingsEntityToBookingsDto).collect(Collectors.toList());
+    public static List<BookingDto> mapBookingListEntityToBookingListDto(List<Booking> bookingList) {
+        return bookingList.stream()
+                .map(Utils::mapBookingsEntityToBookingsDto)
+                .collect(Collectors.toList());
     }
-
 }
